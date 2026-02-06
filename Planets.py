5bf7e47e0ecbf6a1490d607754c5a87a07db46bf
timothy <timothy.imannuel@binus.ac.id>
2026-02-06 22:27:12 +0700

    Commit #1 CompPhys

---
+++ Planets.py
@@ -0,0 +1,32 @@
+import pygame
+import math
+
+pygame.font.init()
+
+#pygame window size 1000x1000
+WIDTH = 1300
+HEIGHT = 1300
+
+#create class for different planets
+class Planet:
+    AU = 149597870700 #Astronomical unit in meters
+    G = 6.6743 * 10e-11 #Gravitational constant in m^3 kg^-1 s^-2
+    SCALE = 100 / AU # 1 AU = 250 pixels
+    TIME = 3600 * 24 #1 day rotation
+
+    def __init__(self, x, y, radius, mass, color): #initialize class
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.mass = mass
+        self.color = color
+        self.is_sun = False
+        self.distance_to_sun = 0
+        self.y_velocity = 0
+        self.x_velocity = 0
+        self.orbit = []
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
---
+++ main.py
@@ -0,0 +1,51 @@
+import pygame
+from Planets import Planet, WIDTH, HEIGHT
+pygame.init()
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Orbit")
+
+BG = (255, 255, 255)
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    sun = Planet(0, 0, 30, 1.988416 * (10 ** 30), (255, 255, 0))
+    sun.sun = True
+
+    earth = Planet(-1 * Planet.AU, 0, 16, 5.9722 * (10 ** 24), (0, 0, 255))
+    earth.y_vel = 29.783 * 1000
+
+    venus = Planet(0.72 * Planet.AU, 0, 14, 4.8673 * (10 ** 24), (255, 255, 191))
+    venus.y_vel = 3.5020 * 10 ** 4
+
+    mercury = Planet(0.40 * Planet.AU, 0, 8, 3.3010 * (10 ** 23), (134, 119, 95))
+    mercury.y_vel = 4.7362 * (10 ** 4)
+
+    mars = Planet(1.50 * Planet.AU, 0, 11, 6.4169 * (10 ** 23), (198, 123, 92))
+    mars.y_vel = 24.077 * 1000
+
+    jupiter = Planet(5.2 * Planet.AU, 0, 40, 1.8981 * (10 ** 27), (166, 138, 114))
+    jupiter.y_vel = 13.07 * 1000
+
+    planets = [sun, earth, venus, mercury, mars, jupiter]
+
+    while run:
+        clock.tick(60)
+        #WIN.fill(BG)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        for planet in planets:
+            planet.draw(WIN)
+
+        pygame.display.update()
+
+    pygame.quit()
+
+main()
+
