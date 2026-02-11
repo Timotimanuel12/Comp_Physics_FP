132c444227914fdf506eb4db4e3383aa6e3ac9c0
timothy <timothy.imannuel@binus.ac.id>
2026-02-11 21:22:28 +0700

    force of attraction done

--- Planets.py
+++ Planets.py
@@ -9,8 +9,8 @@ HEIGHT = 1300
 
 #create class for different planets
 class Planet:
-    AU = 149597870700 #Astronomical unit in meters
-    G = 6.6743 * 10e-11 #Gravitational constant in m^3 kg^-1 s^-2
+    AU = 149.6e6 * 1000 #Astronomical unit in meters
+    G = 6.67428e-11 #Gravitational constant in m^3 kg^-1 s^-2
     SCALE = 250 / AU # 1 AU = 250 pixels
     TIME = 3600 * 24 #1 day rotation
 
@@ -25,6 +25,40 @@ class Planet:
         self.y_velocity = 0
         self.x_velocity = 0
         self.orbit = []
+
+    def force_of_attraction(self, other):
+        other_x = other.x
+        other_y = other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt((distance_x ** 2) + (distance_y ** 2))
+        force = (self.G * self.mass * other.mass) / distance ** 2 #force of attraction formula
+        angle = math.atan2(distance_y, distance_x)
+        x_force = math.cos(angle) * force
+        y_force = math.sin(angle) * force
+
+        if other.is_sun:
+            self.distance_to_sun = distance
+
+        return x_force, y_force
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy = self.force_of_attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_velocity += total_fx / self.mass * self.TIME
+        self.y_velocity += total_fy / self.mass * self.TIME
+
+        self.x += self.x_velocity * self.TIME
+        self.y += self.y_velocity * self.TIME
+        self.orbit.append((self.x, self.y))
+
 
     def draw(self, win):
         x = self.x * self.SCALE + WIDTH / 2
--- main.py
+++ main.py
@@ -12,40 +12,37 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0, 40, 1.988416 * (10 ** 30), (255, 255, 0))
+    sun = Planet(0, 0, 30, 1.988416 * (10 ** 30), (255, 255, 0))
     sun.sun = True
 
     # Earth Radius 15
-    earth = Planet(-1 * Planet.AU, 0, 15, 5.9722 * (10 ** 24), (0, 0, 255))
-    earth.y_vel = 29.783 * 1000
+    earth = Planet(-1 * Planet.AU, 0, 16, 5.9722 * (10 ** 24), (0, 0, 255))
+    earth.y_velocity = 29.783 * 1000
 
     # Venus Radius 12
-    venus = Planet(0.72 * Planet.AU, 0, 12, 4.8673 * (10 ** 24), (255, 255, 191))
-    venus.y_vel = 3.5020 * 10 ** 4
+    venus = Planet(0.72 * Planet.AU, 0, 14, 4.8673 * (10 ** 24), (255, 255, 191))
+    venus.y_velocity = 3.5020 * 10 ** 4
 
     # Mercury Radius 8
     mercury = Planet(0.40 * Planet.AU, 0, 8, 3.3010 * (10 ** 23), (134, 119, 95))
-    mercury.y_vel = 4.7362 * (10 ** 4)
+    mercury.y_velocity = 4.7362 * (10 ** 4)
 
     # Mars Radius 10
-    mars = Planet(1.50 * Planet.AU, 0, 10, 6.4169 * (10 ** 23), (198, 123, 92))
-    mars.y_vel = 24.077 * 1000
-
-    # # Jupiter Radius 30 (Big, but smaller than Sun)
-    # jupiter = Planet(5.2 * Planet.AU, 0, 30, 1.8981 * (10 ** 27), (166, 138, 114))
-    # jupiter.y_vel = 13.07 * 1000
+    mars = Planet(1.50 * Planet.AU, 0, 12, 6.4169 * (10 ** 23), (198, 123, 92))
+    mars.y_velocity = 24.077 * 1000
 
     planets = [sun, earth, venus, mercury, mars]
 
     while run:
         clock.tick(60)
-        #WIN.fill(BG)
+        WIN.fill((0,0,0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
 
         pygame.display.update()
